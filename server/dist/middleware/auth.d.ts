3cd46092b91f3cecc4793885ddee4b8fc304f5a6
frogmanjhb <seancreamer62@gmail.com>
2025-10-14 21:20:28 +0200

    CRITICAL FIX: Remove old dist folder and clean build process

--- server/dist/middleware/auth.d.ts
+++
@@ -1,8 +0,0 @@
-import { Request, Response, NextFunction } from 'express';
-import { User } from '../types';
-export interface AuthenticatedRequest extends Request {
-    user?: User;
-}
-export declare const authenticateToken: (req: AuthenticatedRequest, res: Response, next: NextFunction) => Promise<Response<any, Record<string, any>> | undefined>;
-export declare const requireRole: (roles: string[]) => (req: AuthenticatedRequest, res: Response, next: NextFunction) => Response<any, Record<string, any>> | undefined;
-//# sourceMappingURL=auth.d.ts.map
